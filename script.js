7a346bb6e70edb933aafa3e3ad6aee9c9fc0d2b8
bluinha <cecyliahelenna@gmail.com>
2025-08-19 21:37:50 -0300

    mecanica de hatch e morte feitas

--- script.js
+++ script.js
@@ -11,29 +11,72 @@ function typeWriter(element, text, speed = 80) {
     typing();
 }
 
-function loading() {
-    let msg = document.getElementById("msg");
-    let pic = document.getElementById("pic");
-    let date = new Date();
-    let hour = date.getHours();
-    msg.innerHTML = `Agora são ${hour} horas`;
+class Pet {
+    constructor(name, birthDate, hungerStatus = 0, sprite) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.hungerStatus = hungerStatus;
+        this.maxAge = 7;
+        this.maxHunger = 100;
+        this.sprite = sprite;
+    }
 
-    if (hour >= 0 && hour < 12) {
-        //bom dia
-        typeWriter(kumo, "kumo diz bom dia!");
-        pic.src = "imgs/caracol.gif";
-        document.body.style.background = "#0391fd";
-    } else if (hour >= 12 && hour < 18) {
-        //boa tarde
-        typeWriter(kumo, "kumo diz boa tarde!");
-        pic.src = "imgs/caracol.gif";
-        document.body.style.background = "#f5bb3f";
-    } else {
-        //boa noite
-        typeWriter(kumo, "kumo está dormindo");
-        pic.src = "imgs/caracolSleep.gif";
-        document.body.style.background = "#040f99";
+    getAge() {
+        let years = new Date() - new Date(this.birthDate);
+        return Math.floor(years / (1000 * 60 * 60 * 24));
+    }
+
+    isAlive() {
+        //it dies of age or hungry
+        if (this.getAge() >= this.maxAge) return false;
+        if (this.hungerStatus <= 0) return false;
+
+        return true;
     }
 }
 
-window.onload = loading();
+// == INITIAL STATE == //
+const hatch = document.getElementById("hatch-btn");
+const sprite = document.getElementById("sprite");
+let isHatched = JSON.parse(localStorage.getItem("hatchedState")) || false;
+
+//== HACHING EVENTS ==//
+hatch.addEventListener("click", () => {
+    //shows the pet
+    sprite.src = "imgs/caracol.gif";
+
+    //creates the object pet
+    let today = new Date();
+    let pet = new Pet("kumo", today, 0, sprite);
+
+    //saves the pet in the storage
+    localStorage.setItem("pet", JSON.stringify(pet));
+
+    //changes the state
+    isHatched = true;
+    localStorage.setItem("hatchedState", JSON.stringify(isHatched));
+
+    hatch.style.display = "none";
+});
+
+//== LOADING EVENTS ==//
+function loadPet() {
+    if (localStorage.getItem("pet")) {
+        //recreates the pet
+        savedPet = localStorage.getItem("pet");
+        const data = JSON.parse(savedPet);
+        let pet = new Pet(data.name, data.birthDate, data.hungerStatus, data.sprite);
+
+        if (!pet.isAlive()) {
+            alert("oh não, o kumo morreu!");
+            localStorage.removeItem("pet");
+            localStorage.removeItem("hatchedState");
+            return;
+        }
+        //recreates the pet visually
+        sprite.src = pet.sprite;
+        hatch.style.display = "none";
+    }
+}
+
+window.onload = loadPet;
